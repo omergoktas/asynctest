528b35030033dd948263d71f0d15fcc96682e497
Ömer Göktaş <omer@omergoktas.com>
2019-02-17 18:54:50 +0300

    v1.0
    Signed-off-by: Ömer Göktaş <omer@omergoktas.com>

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 ** Copyright (C) 2019 Ömer Göktaş
 ** Contact: omergoktas.com
 **
-** This file is part of the AsyncTest library.
+** This file is part of the AsyncTest project.
 **
 ** The AsyncTest is free software: you can redistribute it and/or
 ** modify it under the terms of the GNU Lesser General Public License
@@ -21,67 +21,85 @@
 ****************************************************************************/
 
 #include <async.h>
-#include <iostream>
+#include <QRandomGenerator>
+#include <QApplication>
+#include <QProgressDialog>
+#include <QMessageBox>
+#include <QFutureWatcher>
+#include <QTimer>
 
-struct Foo
+// Example task state messages
+static const QStringList messages = {"Initializing...", "Phase 1/2", "Phase 2/2", "Finalizing..."};
+
+// Example task function
+int timeConsumingRandomNumberGenerator(QFutureInterfaceBase* futureInterface, int rangeMin, int rangeMax)
 {
-    void hello() { std::cout << "hello" << std::endl; }
-    void hello(const char* msg) { std::cout << msg << std::endl; }
-};
+    auto future = static_cast<QFutureInterface<int>*>(futureInterface);
+    future->setProgressRange(0, 100);
+    future->setProgressValue(0);
 
-
-int callback(QFutureInterfaceBase* future, std::string e) {
-    std::cout << e << std::endl;
-    return {};
-}
-
-int callback2(QFutureInterfaceBase* future) {
-//    std::cout << e << std::endl;
-    return {};
-}
-
-
-struct A {
-    int a(std::future<int>& future, std::string e) {
-        std::cout << e << std::endl;
-        return {};
+    int value = QRandomGenerator::global()->bounded(rangeMin, rangeMax);
+    for (int i = 1; i <= 100; ++i) {
+        if (future->isPaused())
+            future->waitForResume();
+        if (future->isCanceled())
+            return value;
+        value = QRandomGenerator::global()->bounded(rangeMin, rangeMax);
+        future->setProgressValueAndText(i, QString("Random number: %1").arg(value));
+        QThread::msleep(50);
     }
 
-    int b(QFutureInterfaceBase* future, std::string e) {
-        std::cout << e << std::endl;
-        return {};
-    }
-};
+    future->reportResult(value);
+    return value;
+}
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication app(argc, argv);
-    std::function<void(void)> ss;
-    A a;
-    Async::run([] () { qDebug() << "adasd"; });
-    Async::run([] (int) { qDebug() << "adasd 1"; }, 6);
-    Async::run([] (int) { qDebug() << "adasd 2"; return 2;}, 6);
-    Async::run([] (int e) { qDebug() << "adasd" << e; return 2;}, 3);
-    Async::run([] (int e, int k) { qDebug() << "adasd" << e << k; return 2;}, 4, 444);
-    Async::run([] (int e, char c) { qDebug() << "adasd" << e << c; return c;}, 5, '@');
+    //! Some configurations regarding to GUI (nothing to do with Async lib)
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QApplication app(argc, argv);
+    QApplication::setStyle("fusion");
 
-    Async::run([] (QFutureInterfaceBase*, auto x, auto y) { return x + y; }, 25, 54);
-    Async::run([] (QFutureInterfaceBase* f) { qDebug() << f; return 9; });
-    Async::run(std::bind([] () { return "sds"; }));
-    Async::run(std::bind([] (QFutureInterfaceBase*) { return "sds"; }, std::placeholders::_1));
-    Async::run([] (QFutureInterfaceBase*) { return "sds"; });
-    Async::run([] (QFutureInterfaceBase*, char i) { return "sds"; }, 'd');
-    Async::run([] (QFutureInterfaceBase*) -> int {});
-    Async::run([] (QFutureInterfaceBase*) {});
-    Async::run(std::bind<int>([] () -> int {}));
-    Async::run(callback2);
-    Async::run(&callback2);
-    Async::run(callback, std::string("naber 4 yavrum 1"));
-    Async::run(&callback, std::string("naber 4 yavrum 1"));
-    Async::run(std::function<int(QFutureInterfaceBase*, std::string)>(callback), std::string("naber 4 yavrum 2"));
-    Async::run(std::bind(&A::b, &a, std::placeholders::_1, std::placeholders::_2), std::string("naber 4 yavrum 3"));
-    Async::run(std::bind(&A::b, &a, std::placeholders::_1, std::string("naber 4 yavrum 4")));
-    Async::run(std::bind(&A::b, &a, std::placeholders::_1, std::string("naber 4 yavrum 4.1")), std::string("dd"));
+    QProgressDialog dialog;
+    dialog.setAutoClose(false);
+    dialog.setAutoReset(false);
+    dialog.setMinimumWidth(250);
+    dialog.setWindowTitle("Async Test");
+    dialog.setCancelButtonText("Pause");
+    dialog.disconnect(&dialog); // Hack: Prevent "rejected" signal to close dialog window
+    dialog.show();
+
+    //! Run the task asynchronously
+    QFutureWatcher<int> watcher;
+    watcher.setFuture(Async::run(timeConsumingRandomNumberGenerator, 100, 999));
+
+    //! Using "canceled" signal of the dialog in order to provide pause/resume functionality
+    QObject::connect(&dialog, &QProgressDialog::canceled, [&] {
+        if (watcher.isFinished()) {
+            dialog.close();
+            return;
+        }
+        watcher.future().togglePaused();
+        dialog.setLabelText(watcher.isPaused() ? "Paused" : "Running");
+        dialog.setCancelButtonText(watcher.isPaused() ? "Resume" : "Pause");
+    });
+
+    //! Catch state changes on the task by connecting appropriate signals to slots
+    QObject::connect(&watcher, &QFutureWatcherBase::progressTextChanged, [&]
+    { dialog.setLabelText(watcher.progressText()); });
+    QObject::connect(&watcher, &QFutureWatcherBase::progressValueChanged, [&]
+    { dialog.setValue(watcher.progressValue()); });
+    QObject::connect(&watcher, &QFutureWatcherBase::resultReadyAt, [&]
+    { QMessageBox::information(0, "Done", "Result: " + QString::number(watcher.result())); });
+    QObject::connect(&watcher, &QFutureWatcherBase::finished, [&] {
+        dialog.setLabelText("Done!");
+        dialog.setCancelButtonText("Close");
+    });
+    // See QTBUG-12152 for QFutureWatcherBase::paused signal
+
+    //! Cancel the operation after 1 second
+    // QTimer::singleShot(1000, &watcher, &QFutureWatcher<int>::cancel);
 
     return app.exec();
 }
